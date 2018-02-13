5563f6b8a9e698f2595ee4105f8fcc809e430e1a
GadzhievSA <s.gadzhiev@id-mt.ru>
2018-02-13 09:21:05 +0300

    Тест создания логгера

--- orm/src/test/java/ru/saidgadjiev/orm/next/core/logger/LoggerFactoryTest.java
+++ orm/src/test/java/ru/saidgadjiev/orm/next/core/logger/LoggerFactoryTest.java
@@ -7,15 +7,16 @@ public class LoggerFactoryTest {
 
     @Test
     public void getLogger() throws Exception {
-        System.setProperty(LoggerFactory.LOG_CLASS_PROPERTY, "ru.saidgadjiev.orm.next.core.logger.TestLog");
+        Log dummyLog = LoggerFactory.getLogger(Test.class);
+
+        Assert.assertTrue(dummyLog instanceof DummyLog);
         System.setProperty(LoggerFactory.LOG_ENABLED_PROPERTY, "true");
-        Log logTest = LoggerFactory.getLogger(Test.class);
-
-        Assert.assertTrue(logTest instanceof TestLog);
-        System.setProperty(LoggerFactory.LOG_CLASS_PROPERTY, "ru.saidgadjiev.orm.next.core.logger.Log4j");
-
         Log log4j = LoggerFactory.getLogger(Test.class);
 
         Assert.assertTrue(log4j instanceof Log4j);
+        System.setProperty(LoggerFactory.LOG_CLASS_PROPERTY, "ru.saidgadjiev.orm.next.core.logger.TestLog");
+        Log logTest = LoggerFactory.getLogger(Test.class);
+
+        Assert.assertTrue(logTest instanceof TestLog);
     }
 }
