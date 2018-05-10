acf5f9c60a4e84b056c2d8f73e5bdeb8659e1921
GadzhievSA <said.gadzhiev@id-mt.ru>
2018-05-10 07:26:18 +0300

    checkstyle

--- up-cache/src/main/java/ru/saidgadjiev/up/cache/core/Cache.java
+++ up-cache/src/main/java/ru/saidgadjiev/up/cache/core/Cache.java
@@ -1,7 +1,19 @@
 package ru.saidgadjiev.up.cache.core;
 
+/**
+ * The Cache interface provide api for caching.
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ *
+ * @author Said Gadjiev
+ */
 public interface Cache<K, V> {
 
+    /**
+     *
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     */
     void put(K key, V value);
 
     void invalidate(K key);
--- up-cache/src/main/java/ru/saidgadjiev/up/cache/core/LRUHashMap.java
+++ up-cache/src/main/java/ru/saidgadjiev/up/cache/core/LRUHashMap.java
@@ -3,12 +3,31 @@ package ru.saidgadjiev.up.cache.core;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Map with lru item remove algorithm.
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ *
+ * @author Said Gadjiev
+ */
 public class LRUHashMap<K, V> extends LinkedHashMap<K, V> {
 
+    /**
+     * Map default load factor.
+     */
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    /**
+     * Map capacity.
+     */
     private final int capacity;
 
+    /**
+     * Constructor for empty LRUHashMap with initial capacity.
+     * @param capacity initial map capacity
+     */
     public LRUHashMap(int capacity) {
-        super(capacity, 0.75F, true);
+        super(capacity, DEFAULT_LOAD_FACTOR, true);
         this.capacity = capacity;
     }
 
