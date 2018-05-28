983dc03714f990c94f6a7effcb159d99e41f7837
GadzhievSA <s.gadzhiev@id-mt.ru>
2018-05-28 10:01:03 +0300

    Фикс java docs + рефакторинг

--- orm-next-core/src/main/java/ru/saidgadjiev/ormnext/core/query/core/function/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains classes for define aggregate functions.
- */
-package ru.saidgadjiev.ormnext.core.query.core.function;
--- orm-next-core/src/main/java/ru/saidgadjiev/ormnext/core/stament_executor/object/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains classes for work with persisted object eg. define lazy initialization.
- */
-package ru.saidgadjiev.ormnext.core.stament_executor.object;
